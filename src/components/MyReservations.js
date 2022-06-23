251c5f9ceecf7ee75ad30f8aaec1bba4a7ce496d
farouqdaif1 <daiffarouq@gamil.com>
2022-06-23 12:38:14 +0200

    Add files for reservations

--- src/components/MyReservations.js
+++ src/components/MyReservations.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const MyReservations = () => (
-  <section>
-    <h1>My Reservation</h1>
-  </section>
-);
+const MyReservations = () => {
+  const url = 'http://[::1]:3000/api/v1/reservation';
+  const [reservation, setReservation] = useState(null);
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setReservation(response.data);
+    });
+  }, []);
+
+  return (
+    <section>
+      <h1>My Reservation</h1>
+      <h2>{reservation}</h2>
+    </section>
+  );
+};
 
 export default MyReservations;

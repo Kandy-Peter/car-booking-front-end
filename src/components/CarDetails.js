bab8a2e4e0beb0fdca1bbcc68dfc2a22f833f3e5
kandy-peter <kandypeter03@gmail.com>
2022-06-27 20:45:23 +0200

    Display remaining card detail

--- src/components/CarDetails.js
+++ src/components/CarDetails.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fecthCars } from '../redux/Cars/cars';
+import Jeep from '../assets/images/7vylxq_large.png';
+import '../styles/card_details.scss';
 
 const CarDetails = () => {
   const cars = useSelector((state) => state.reducers.cars);
@@ -18,7 +20,31 @@ const CarDetails = () => {
     <>
       {selectedCar.map((item) => (
         <div className="car_container" key={item.id}>
-          <p>{item.name}</p>
+          <img src={Jeep} alt={item.name} />
+          <div className="car_details">
+            <h2>{item.name.slice(0, 9)}</h2>
+            <ul>
+              <li>
+                model:
+                {' '}
+                {item.model}
+              </li>
+              <li className="price">
+                price:
+                {' '}
+                {item.per_day_amount}
+                $
+              </li>
+              <li>
+                {item.reserved === false ? (
+                  <li>availability: No available</li>
+                ) : (
+                  <li>availability: available</li>
+                )}
+              </li>
+            </ul>
+            <span>Let us satisfy your desire!</span>
+          </div>
         </div>
       ))}
     </>
